38a823368b386c753606ce398b3b8960dbf9db6f
M Sumitha <sumitha142005@gmail.com>
2026-06-23 14:28:59 +0530

    Update config for Railway

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
 import os
 
 class Config:
-    # Flask secret key (change this in production)
     SECRET_KEY = os.environ.get("SECRET_KEY", "my_secret_key_123")
-
-    # MongoDB Atlas URI
-    MONGO_URI = os.environ.get(
-        "MONGO_URI",
-        "mongodb+srv://sumitha142005_db_user:MSumi1234@cluster0.zxmfqu6.mongodb.net/ecommerce?retryWrites=true&w=majority"
-    )
+    MONGO_URI = os.environ.get("MONGO_URI")
